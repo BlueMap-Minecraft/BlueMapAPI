3949ebcb565ecf44bd106daf69582d76d8cfbca1
Lukas Rieger (Blue) <TBlueF@users.noreply.github.com>
2022-10-13 21:46:41 +0200

    Tentative JitPack build fix 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,3 +94,15 @@ tasks.processResources {
         )
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}
