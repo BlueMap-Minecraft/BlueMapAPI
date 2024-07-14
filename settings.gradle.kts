c16c16a9cdc257a53829c5c6efd9411908afa3c6
Lukas Rieger (Blue) <github@bluecolored.de>
2024-07-15 00:18:49 +0200

    Rework gradle-build setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
-rootProject.name = "BlueMapAPI"
+rootProject.name = "bluemap-api"
+
+logger.lifecycle("""
+## Building BlueMapAPI ...
+Java: ${System.getProperty("java.version")}
+JVM: ${System.getProperty("java.vm.version")} (${System.getProperty("java.vendor")})
+Arch: ${System.getProperty("os.arch")} 
+""")
