40f0724dfbfbc9eac7f3f4296544ff456fd7f568
Lukas Rieger (Blue) <github@bluecolored.de>
2024-07-15 00:30:24 +0200

    Fix versioning

--- buildSrc/src/main/kotlin/versioning.kt
+++ buildSrc/src/main/kotlin/versioning.kt
@@ -36,7 +36,8 @@ private fun Project.runCommand(cmd: String, fallback: String? = null): String {
         }
         .run {
             val error = errorStream.bufferedReader().readText().trim()
-            if (error.isEmpty()) inputStream.bufferedReader().readText().trim()
+            if (error.isEmpty()) return inputStream.bufferedReader().readText().trim()
+            logger.warn("Failed to execute command '$cmd': $error")
             if (fallback != null) return fallback
             throw IOException(error)
         }
