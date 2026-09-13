6aa648035fd3058df004fbc6de46e0bf2601f99f
Lukas Rieger <github@bluecolored.de>
2026-09-13 15:04:56 +0200

    Update gradle wrapper, libraries and add dependabot config

--- buildSrc/src/main/kotlin/bluemap.base.gradle.kts
+++ buildSrc/src/main/kotlin/bluemap.base.gradle.kts
@@ -58,7 +58,7 @@ spotless {
         target ("src/*/java/**/*.java")
 
         licenseHeaderFile(rootProject.file("LICENSE_HEADER"))
-        indentWithSpaces()
+        leadingTabsToSpaces(4)
         trimTrailingWhitespace()
     }
 }
